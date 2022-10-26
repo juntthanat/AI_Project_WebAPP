6e39898680a70754db4c4917ab7620264d2b6935
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2022-10-26 10:43:01 +0000

    feat: finish Simple design

--- AI_Project_WebAPP/src/App.tsx
+++ AI_Project_WebAPP/src/App.tsx
@@ -12,8 +12,25 @@ function App() {
           Game Title
         </div>
         <div id="game-container-body">
-          <div id="input-container">
-            
+          <div id="input-container-header">
+            <div id="left-input">
+              <form>
+                <input className="user-input" type="text"></input>
+                <input className="user-input" type="text"></input>
+                <input className="user-input" type="text"></input>
+                <input className="user-input" type="text"></input>
+              </form>
+            </div>
+            <div id="right-input">
+              <form>
+                <input className="user-input" type="text" placeholder='Target'></input>
+              </form>
+            </div>
+          </div>
+          <div id="input-container-footer">
+            <form>
+              <input id="submit-user-input" type="button" value="Submit"></input>
+            </form>
           </div>
         </div>
         <div id="game-container-footer"></div>
